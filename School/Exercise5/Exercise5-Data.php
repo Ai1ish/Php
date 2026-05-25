3ad13085a346ac01b7a729e8a367772651234344
Sai <narvasa.ailish@gmail.com>
2026-05-26 01:55:44 +0800

    Add menu database for Class Activity 5

---
+++ School/Exercise5/Exercise5-Data.php
@@ -0,0 +1,37 @@
+<?php
+// Menu database for Class Activity 5
+
+$menu = [
+    "Main Dishes" => [
+        "Pasta" => [
+            "carbonara" => ["name" => "Creamy Carbonara", "price" => 220],
+            "bolognese" => ["name" => "Classic Bolognese", "price" => 190],
+            "pesto"     => ["name" => "Basil Pesto", "price" => 200]
+        ],
+        "Rice Bowls" => [
+            "chicken_teriyaki" => ["name" => "Chicken Teriyaki Bowl", "price" => 240],
+            "beef_gyudon"      => ["name" => "Beef Gyudon", "price" => 260],
+            "tonkatsu"         => ["name" => "Pork Tonkatsu Bowl", "price" => 230]
+        ]
+    ],
+    "Drinks" => [
+        "Coffee" => [
+            "espresso"      => ["name" => "Espresso", "price" => 120],
+            "spanish_latte" => ["name" => "Spanish Latte", "price" => 185],
+            "vietnamese"    => ["name" => "Vietnamese Coffee", "price" => 180]
+        ],
+        "Milk Tea" => [
+            "okinawa"     => ["name" => "Okinawa Milk Tea", "price" => 130],
+            "wintermelon" => ["name" => "Wintermelon Milk Tea", "price" => 130],
+            "matcha_latte" => ["name" => "Uji Matcha Latte", "price" => 150]
+        ]
+    ],
+    "Desserts" => [
+        "Cakes" => [
+            "cheesecake" => ["name" => "Blueberry Cheesecake", "price" => 190],
+            "red_velvet" => ["name" => "Red Velvet Cake", "price" => 175],
+            "choco_fudge"=> ["name" => "Chocolate Fudge", "price" => 160]
+        ]
+    ]
+];
+?>
