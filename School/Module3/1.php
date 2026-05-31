7dd392a50f90a1f45e0fa45c9b07b79540873205
Sai <narvasa.ailish@gmail.com>
2026-05-31 22:45:46 +0800

    Add PHP script to display fruit array

---
+++ School/Module3/1.php
@@ -0,0 +1,12 @@
+<?php
+  $fruit = array("orange", "apple", "grapes", "banana");
+  
+  echo "<pre>";
+  print_r($fruit); 
+  echo "</pre>";
+  
+  // var_dump gives more detailed information including data types
+  echo "<pre>";
+  var_dump($fruit);
+  echo "</pre>";
+?>
