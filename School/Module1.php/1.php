59b406760b6dd5903c34599566b1f2e2ab3ecb05
Sai <narvasa.ailish@gmail.com>
2026-05-31 22:40:38 +0800

    Create 1.php

---
+++ School/Module1.php/1.php
@@ -0,0 +1,10 @@
+<?php
+  // using echo
+  echo "<p>The ABC alphabet</p>";
+  
+  // using print statement
+  print("<p>The ABC alphabet</p>");
+  
+  // using printf statement
+  printf("<p>%s</p>", "The ABC alphabet");
+?>
